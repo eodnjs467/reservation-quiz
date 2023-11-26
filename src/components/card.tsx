d330c5d563de168d3597dd49e19c32c315b0ff1e
Big One <sponjjanc@naver.com>
2023-11-26 23:59:12 +0900

    feat: 예약 조회 화면 Card UI

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,33 +1,53 @@
 import {useNavigate} from "react-router-dom";
+import {useReservationDispatch} from "../context/reservation-context";
 
 export default function Card(
   {
     id, name, phone, date, time, guest, table, description
   }: {
-    id: number, name: string; phone:string; date: string; time: string, guest: string, table: string, description: string
+    id: number, name: string; phone:string; date: string; time: string, guest: number, table: string, description: string
   }) {
+
+  const dispatch  = useReservationDispatch()
   const navigate = useNavigate();
   const onClickEdit = () => {
-    navigate('edit/:id');
+    navigate(`edit/${id}`);
+  }
+  const onDelete = () => {
+    dispatch({type: 'DELETE', id});
+  }
+  // TODO: onSeated 기능 알아보고 작성
+  const onSeated = () => {
+    console.log('seated');
+
   }
   return(
-      <div>
-        <div>
+      <div className={"m-3"}>
+        <div className={"flex"}>
           <h1>{name}</h1>
-          <span><img src={'assets/phone.svg'} alt={'phone'} /> {phone}</span>
+          <div className={"flex"}>
+            <img src={'/assets/phone.svg'} alt={'phone'} />
+            <span>{phone}</span>
+          </div>
         </div>
-        <div>
-          <img src={'assets/today.svg'} alt={'date'} />
+        <div className={"flex"}>
+          <img src={'/assets/today.svg'} alt={'date'} />
           <span>{date}, {time}</span>
         </div>
-        <img src={'assets/group.svg'} alt={'people'} />
-        {guest}
+        <div className={"flex"}>
+          <img src={'/assets/group.svg'} alt={'people'} />
+          {guest}
+        </div>
         {table &&
           <p>Reserved Table {table} , Floor 1</p>
         }
-        <div>
+        <div className={"flex"}>
           {description}
           <img src={'/assets/edit.svg'} alt={'edit'} onClick={onClickEdit}/>
+        </div>
+        <div className={"flex"}>
+          <img className={"border rounded-lg p-3 mr-5"} src={'/assets/trash.svg'} alt={'delete'} onClick={onDelete} />
+          <button className={"border rounded-lg w-full"} onClick={onSeated}>Seated</button>
         </div>
       </div>
   )
