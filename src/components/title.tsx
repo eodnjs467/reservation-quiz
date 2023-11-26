9d63895f3168bd295543f4f0b613e1be9aca3b89
Big One <sponjjanc@naver.com>
2023-11-27 00:00:58 +0900

    fix: 이미지 주소 수정

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -14,14 +14,14 @@ export default function Title({ addButton, title }: {
         <div className={'border rounded-lg'}>
           {addButton ? (
               <div className={"flex m-3"} onClick={handleAddClick}>
-                <img src={"assets/add.svg"} alt={"add"}/>
+                <img src={"/assets/add.svg"} alt={"add"}/>
                 <span className={"text-orange-500"}>New Reservation</span>
               </div>
-          ): <img src={"assets/keyboard_backspace.svg"} alt={"back"} onClick={() => navigate(-1)}/>}
+          ): <img src={"/assets/keyboard_backspace.svg"} alt={"back"} onClick={() => navigate(-1)}/>}
         </div>
         <h1 className={"text-2xl"}>{title}</h1>
         <div>
-          <img src={"assets/close.svg"} alt={'closed'}/>
+          <img src={"/assets/close.svg"} alt={'closed'}/>
         </div>
       </div>
   );
