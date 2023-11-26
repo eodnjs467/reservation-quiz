0c41ccdeb3cc2ad2c9943d490ba58d42b18ce72f
Big One <sponjjanc@naver.com>
2023-11-26 23:58:31 +0900

    feat: 예약 조회 화면 리스트 출력

--- src/pages/reservation.tsx
+++ src/pages/reservation.tsx
@@ -1,9 +1,25 @@
 import Title from "../components/title";
+import {useReservationState} from "../context/reservation-context";
+import Card from "../components/card";
 
 export default function Reservation() {
+  const reservations = useReservationState();
   return (
       <>
         <Title title={'Reservation'} addButton={true}/>
+        <div className={"grid grid-cols-3"}>
+        {reservations.map(reservation => <Card
+            key={reservation.id}
+            id={reservation.id}
+            name={reservation.name}
+            phone={reservation.phone}
+            date={reservation.date}
+            time={reservation.time}
+            guest={reservation.guests}
+            table={reservation.table}
+            description={reservation.description}
+        />)}
+        </div>
       </>
   );
 }
