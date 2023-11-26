e02c314b3fd07f181eed01e1fc2f0e2e17586122
Big One <sponjjanc@naver.com>
2023-11-26 19:17:42 +0900

    feat: router-dom 을 사용하여 페이지 이동을 구현했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Reservation from "./pages/reservation";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import NewReservation from "./pages/new-reservation";
+import EditReservation from "./pages/edit-reservation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Reservation />}/>
+          <Route path={"/add-reservation"} element={<NewReservation />}/>
+          <Route path={"/edit-reservation"} element={<EditReservation />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
